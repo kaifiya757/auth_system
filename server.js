da252482b20110b79290475301a7eeeca1d0a09b
MD <mohammedsabnsabn@gmail.com>
2026-04-09 12:08:59 +0530

    fix

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-require("dotenv").config();
+const express = require("express");
+const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
